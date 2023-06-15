02797484e80cdbe0e9972afe7c586a54e91d498d
Ahmed Shafiuddin <112600024+ahmed23shaf@users.noreply.github.com>
2023-06-15 00:40:41 -0500

    Initial Implementation

--- conversion.h
+++ conversion.h
@@ -1,16 +1,28 @@
 #ifndef CONVERSION_H
 #define CONVERSION_H
 
-#include <map>
 #include <string>
+#include <map>
+#include <iostream>
 
+extern const std::map<std::string, char> binaryToHex;
 extern const std::map<char, std::string> hexLookUp;
 
-int twosComplementToDecimal(int input);
+// INPUT: Reference to a binary string
+// OUTPUT: None
+void TwosComplementOperation(std::string& binaryInput);
 
-int decimalToTwosComplement(int input);
+// INPUT:  A binary number assumed to be in Two's Complement  
+// OUTPUT: The converted number into decimal 
+int twosComplementToDecimal(std::string input);
 
-std::string twosComplementToHex(int input);
+// INPUT: Decimal number (base 10)
+// OUTPUT: The number in binary (Two's Complement Representation)
+std::string decimalToTwosComplement(int input);
+
+// INPUT: A binary number in Two's Complement (as a std::string)
+// OUTPUT: A hexadecimal output (also as std::string)
+std::string twosComplementToHex(std::string input);
 
 int hexToTwosComplement(std::string input);
 
